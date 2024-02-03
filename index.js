284da05f3ebba748b993cefeab2fbd0a84fe42c7
atpursuit <140565742+atpursuit@users.noreply.github.com>
2024-02-03 15:46:10 -0500

    index.js

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
 ````
-
-// We use a 'Map' called 'favoriteColors' to store the favorite colors of Alice, Bob and Charlie.
-let favoriteColors = new Map();
-
-// Here we are using 'set' to add entries. Associating each color to a name.
-favoriteColors.set('Alice', 'Blue');
-favoriteColors.set('Bob', 'Green');
-favoriteColors.set('Charlie', 'Red');
-
-// Listed below is how we get and print the favorite colors of Alice, Bob and Charlie.
-let aliceColor = favoriteColors.get('Alice');
-console.log("Alice's favorite color is " + aliceColor);
-
-let bobColor = favoriteColors.get('Bob');
-console.log("Bob's favorite color is " + bobColor);
-
-let charlieColor = favoriteColors.get('Charlie');
-console.log("Charlie's favorite color is " + charlieColor);
-
-// You're done! This is how we use the 'Map' function.
+  const numbers = [1, 2, 3, 4, 5];
+  const doubledNumbers = numbers.map(num => num * 2);
+  console.log(doubledNumbers);
 
 ````
